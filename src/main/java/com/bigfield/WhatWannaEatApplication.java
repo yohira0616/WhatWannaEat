5b53a79fdcec3d7c8833e56f8b3b3ba44dbd7e00
yohira0616 <yukito.sys.so@gmail.com>
2016-09-25 23:51:47 +0900

    first commit

---
+++ src/main/java/com/bigfield/WhatWannaEatApplication.java
@@ -0,0 +1,12 @@
+package com.bigfield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatWannaEatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatWannaEatApplication.class, args);
+	}
+}
