4fabb0ccc6da6459e4f8345ff1f64903c87839b3
yohira0616 <yukito.sys.so@gmail.com>
2016-09-27 00:16:38 +0900

    ページのスタイル調整

--- src/main/resources/gulpfile.js
+++ src/main/resources/gulpfile.js
@@ -5,7 +5,13 @@ gulp.task('default', function () {
         .pipe(gulp.dest('../../../build/resources/main/templates'));
 });
 
+gulp.task('static', function () {
+    return gulp.src('static/**/*')
+        .pipe(gulp.dest('../../../build/resources/main/static'));
+});
+
 gulp.task('watch', function () {
     gulp.watch('templates/**/*.html', ['default']);
+    gulp.watch('static/**/*', ['static']);
 
 });
