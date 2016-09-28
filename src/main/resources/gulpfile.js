fe1377b6d2f5d17acecc16f46007dee58c7201ce
yohira0616 <yukito.sys.so@gmail.com>
2016-09-28 23:33:03 +0900

    jsファイル

--- src/main/resources/gulpfile.js
+++ src/main/resources/gulpfile.js
@@ -5,6 +5,18 @@ gulp.task('default', function () {
         .pipe(gulp.dest('../../../build/resources/main/templates'));
 });
 
+gulp.task('lib:js', function () {
+    return gulp.src('bower_components/**/*.js')
+        .pipe(gulp.dest('static/scripts/lib'))
+        .pipe(gulp.dest('../../../build/resources/main/static/scripts/lib'));
+});
+
+gulp.task('lib:css', function () {
+    return gulp.src('bower_components/**/*.css')
+        .pipe(gulp.dest('static/scripts/lib'))
+        .pipe(gulp.dest('../../../build/resources/main/static/scripts/lib'));
+});
+
 gulp.task('static', function () {
     return gulp.src('static/**/*')
         .pipe(gulp.dest('../../../build/resources/main/static'));
@@ -13,5 +25,4 @@ gulp.task('static', function () {
 gulp.task('watch', function () {
     gulp.watch('templates/**/*.html', ['default']);
     gulp.watch('static/**/*', ['static']);
-
 });
